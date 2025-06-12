7c4cc92f26e26903c776d08a94ffbbdaa9b9480c
alifcdg <alifnaufal9d@gmail.com>
2025-06-12 14:00:52 +0800

    Inisialisasi untuk 5 nama

--- hallo.cpp
+++ hallo.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//int main() {
+  //cout << "PERUBAHAN!";
+  //return 0;
+//}
+
+//INI ADALAH CODING ARRAY STRING VERSI MANUAL
 int main() {
-  cout << "PERUBAHAN!";
+  string nama1, nama2, nama3, nama4, nama5;
+  cout << "Masukkan 5 nama sesuai keperluan anda: \n";
   return 0;
 }
