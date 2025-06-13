6a32695313b58ab6bf0d90842fccfd058f010a91
alifcdg <alifnaufal9d@gmail.com>
2025-06-13 20:55:27 +0800

    Memasukkan data 5 buah nama untuk string array

--- hallo.cpp
+++ hallo.cpp
@@ -10,5 +10,18 @@ using namespace std;
 int main() {
   string nama1, nama2, nama3, nama4, nama5;
   cout << "Masukkan 5 nama sesuai keperluan anda: \n";
+  
+  cout << "Nama 1: ";
+  (cin, nama1);
+  cout << "Nama 2: ";
+  (cin, nama2);
+  cout << "Nama 3: ";
+  (cin, nama3);
+  cout << "Nama 4; ";
+  (cin, nama4);
+  cout << "Nama 5; ";
+  (cin, nama5);
+
+  
   return 0;
 }
